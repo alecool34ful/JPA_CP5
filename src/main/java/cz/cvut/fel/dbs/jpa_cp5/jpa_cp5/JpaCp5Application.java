07c8242c21474a626c91c513a6ef0ccf0aa9a843
Pavel Smiga <smigapav@fel.cvut.cz>
2023-05-13 17:24:46 +0200

    Initial commit

---
+++ src/main/java/cz/cvut/fel/dbs/jpa_cp5/jpa_cp5/JpaCp5Application.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.dbs.jpa_cp5.jpa_cp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCp5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaCp5Application.class, args);
+    }
+
+}
---
+++ src/test/java/cz/cvut/fel/dbs/jpa_cp5/jpa_cp5/JpaCp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.dbs.jpa_cp5.jpa_cp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaCp5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
